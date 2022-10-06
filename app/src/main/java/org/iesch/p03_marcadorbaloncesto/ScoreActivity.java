0704be56706ec737fe4fc4273f70e836e5013f65
marcos <msanchezm@iesch.org>
2022-10-06 17:50:46 +0200

    -Práctica finalizada

---
+++ app/src/main/java/org/iesch/p03_marcadorbaloncesto/ScoreActivity.java
@@ -0,0 +1,34 @@
+package org.iesch.p03_marcadorbaloncesto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.iesch.p03_marcadorbaloncesto.databinding.ActivityScoreBinding;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    private ActivityScoreBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityScoreBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // 1 - Recojo las puntuaciones que vienen del actuvity del marcador
+        int localScore = getIntent().getExtras().getInt("localScore");
+        int visitorScore = getIntent().getExtras().getInt("visitorScore");
+        // 2 - dibujo las puntuaciones obtenidas y escribo el texto
+        binding.scoreText.setText(String.valueOf(localScore) + " - " + String.valueOf(visitorScore));
+
+        if (localScore > visitorScore){
+            binding.whoWonText.setText(" Ganó el equipo local ");
+        } else if (visitorScore > localScore){
+            binding.whoWonText.setText(" Ganó el equipo visitante ");
+        } else {
+            binding.whoWonText.setText(" Ha habido un empate ");
+        }
+
+    }
+}
